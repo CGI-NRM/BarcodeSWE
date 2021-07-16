a7f0603cfa11545ab025cacbceae23e5869f15d2
Elias Lundell <elilun03@gmail.com>
2021-07-16 13:11:06 +0200

    Blast query with more cheks and columns

--- perform_blast_query.sh
+++ perform_blast_query.sh
@@ -1,6 +1,8 @@
+headers="qseqid qgi qacc qaccver qlen sseqid sallseqid sgi sallgi sacc saccver sallacc slen qstart qend sstart send qseq sseq evalue bitscore score length pident nident mismatch positive gapopen gaps ppos frames qframe sframe btop staxid ssciname scomname sblastname sskingdom staxids sscinames scomnames sblastnames sskingdoms stitle salltitles sstrand qcovs qcovhsp qcovus"
 
-headers="scomnames sscinames evalue length \
-sstart send qstart qend mismatch qgi qseqid"
+
+   	    # headers="scomnames scomname sscinames ssciname length stitle sstart send qstart \
+# qend mismatch qgi qseqid gaps staxid sblastname mismatch evalue pident score bitscore"
 
 temp_file=$(mktemp)
 
@@ -8,8 +10,12 @@ blastn -db mito \
 -query blast_query.fa \
 -out $temp_file \
 -num_threads 12 \
--outfmt "10 $headers" \
+-outfmt "6 $headers" \
 -max_target_seqs 500
 
-echo $headers | tr " " , > blast_result.out
+if [[ -e blast_result.out ]]; then
+    rm blast_result.out
+fi
+
+echo $headers | tr " " \\t > blast_result.out
 cat $temp_file >> blast_result.out
